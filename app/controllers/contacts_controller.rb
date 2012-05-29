97b63e9083a15868d893bd307dcb1aeb3348e6c2
Joel Garcia Martinez <joelgarciamartinez@gmail.com>
2012-05-28 21:09:41 -0500

    Using responders instead to simplify controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,11 @@
 class ContactsController < ApplicationController
+  respond_to :html, :json
   # GET /contacts
   # GET /contacts.json
   def index
     @contacts = Contact.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @contacts }
-    end
+    respond_with @contacts
   end
 
   # GET /contacts/1
@@ -15,10 +13,7 @@ class ContactsController < ApplicationController
   def show
     @contact = Contact.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @contact }
-    end
+    respond_with @contact
   end
 
   # GET /contacts/new
@@ -26,10 +21,7 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @contact }
-    end
+    respond_with @contact
   end
 
   # GET /contacts/1/edit
@@ -42,15 +34,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
 
-    respond_to do |format|
-      if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
-        format.json { render json: @contact, status: :created, location: @contact }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with @contact if @contact.save
   end
 
   # PUT /contacts/1
@@ -58,15 +42,7 @@ class ContactsController < ApplicationController
   def update
     @contact = Contact.find(params[:id])
 
-    respond_to do |format|
-      if @contact.update_attributes(params[:contact])
-        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with @contact if @contact.update_attributes(params[:contact])
   end
 
   # DELETE /contacts/1
@@ -75,9 +51,6 @@ class ContactsController < ApplicationController
     @contact = Contact.find(params[:id])
     @contact.destroy
 
-    respond_to do |format|
-      format.html { redirect_to contacts_url }
-      format.json { head :no_content }
-    end
+    respond_with @contact
   end
 end
